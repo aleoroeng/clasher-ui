2c617fdd4957700f7ee720babe424d3c397d9bef
alejandroOro99 <alejandro.orozco@revature.net>
2021-08-14 11:20:10 -0400

    first commit

---
+++ clasher-ui/src/main/java/com/clasher/clasherui/ClasherUiApplication.java
@@ -0,0 +1,13 @@
+package com.clasher.clasherui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClasherUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClasherUiApplication.class, args);
+	}
+
+}
---
+++ clasher-ui/src/test/java/com/clasher/clasherui/ClasherUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clasher.clasherui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClasherUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
