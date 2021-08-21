251e6d373375b441688dfb523c67b678d9f4039c
aleoroeng <oro99eng@gmail.com>
2021-08-21 19:25:42 -0400

    updated ui

---
+++ clasher-ui-angular/src/environments/environment.ts
@@ -0,0 +1,17 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  apitoken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjBjMGE2YzlmLTU3MWQtNDcyYy1hYjdmLTQxOTFiNzc3Zjg3MCIsImlhdCI6MTYyOTE2MjIwMiwic3ViIjoiZGV2ZWxvcGVyLzY1Njg2YjlhLWUyM2MtM2NlMS00NGQ5LTI4M2MyNjc0OGRjMyIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjcwLjEzMS4xNzYuNzkiXSwidHlwZSI6ImNsaWVudCJ9XX0.G3HSgerZTc09fBcksl0w4aDKrppREMBGVVKa2XQCtZLu1YUrxAT8u7D_BHGkMZ6BXH1e7FPVqnZt_cZcyUKGLQ'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
